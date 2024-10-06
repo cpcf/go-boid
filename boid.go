b0d3622881cd92c34422c028af88f8ab56a6dd09
cpcf <piracyonmars@gmail.com>
2024-10-06 11:30:48 +0100

    Loop around screen

--- boid.go
+++ boid.go
@@ -3,9 +3,14 @@ package main
 type boid struct {
 	x, y                 int
 	xVelocity, yVelocity int
+	maxX, maxY           int
 }
 
 func (b *boid) update() {
 	b.x += b.xVelocity
 	b.y += b.yVelocity
+
+	b.x = (b.x + b.maxX) % b.maxX
+	b.y = (b.y + b.maxY) % b.maxY
+
 }
